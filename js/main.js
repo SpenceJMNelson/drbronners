ea38d58e20872a032a8ac4b321471f68e94ea04c
Spence Nelson <spenjmn@me.com>
2017-02-20 15:24:03 -0500

    Initial upload

---
+++ js/main.js
@@ -0,0 +1,50 @@
+$( document ).ready(function() {
+	$( ".peppermint-link" ).click(function() {
+	  	$( "body" ).removeClass();
+	  	$( "body" ).addClass( "peppermint" );
+	});
+
+	$( ".baby-link" ).click(function() {
+	  	$( "body" ).removeClass();
+	  	$( "body" ).addClass( "baby" );
+	});
+
+	$( ".teatree-link" ).click(function() {
+	  	$( "body" ).removeClass();
+	  	$( "body" ).addClass( "teatree" );
+	});
+
+	$( ".lavendar-link" ).click(function() {
+	  	$( "body" ).removeClass();
+	  	$( "body" ).addClass( "lavendar" );
+	});
+
+	$( ".rose-link" ).click(function() {
+	  	$( "body" ).removeClass();
+	  	$( "body" ).addClass( "rose" );
+	});
+
+	$( ".citrus-link" ).click(function() {
+	  	$( "body" ).removeClass();
+	  	$( "body" ).addClass( "citrus" );
+	});
+
+	$( ".almond-link" ).click(function() {
+	  	$( "body" ).removeClass();
+	  	$( "body" ).addClass( "almond" );
+	});
+
+	$( ".eucalyptus-link" ).click(function() {
+	  	$( "body" ).removeClass();
+	  	$( "body" ).addClass( "eucalyptus" );
+	});
+
+	$( ".home-link" ).click(function() {
+	  	$( "body" ).removeClass();
+	});
+
+	$( ".rant-link" ).click(function() {
+	  	$( ".all-one-text-wrapper, .bronners-interface, .main-nav, .all-one-text" ).toggleClass( "showtext" );
+	});
+
+ });
